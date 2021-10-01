2b66a604fa91a577903d3bd86c4d11f98aa718dd
gara73 <91637911+gara73@users.noreply.github.com>
2021-10-01 09:21:29 +0300

    Add files via upload

---
+++ Students.py
@@ -0,0 +1,18 @@
+class Student:
+    __name = ""
+    __group = ""
+        
+    def set_name(self, name):
+        self.__name = name
+        
+    def get_name(self):
+        return self.__name
+
+    def set_group(self, group):
+        self.__group = group
+        
+    def get_group(self):
+        return self.__group
+
+
+
---
+++ Teachers.py
@@ -0,0 +1,18 @@
+class Teacher:
+    __name = ""
+    __qualification = ""
+        
+        
+    def set_name(self, name):
+        self.__name = name
+        
+    def get_name(self):
+        return self.__name
+
+    def set_qualification(self, qualification):
+        self.__qualification = qualification
+        
+    def get_qualification(self):
+        return self.__qualification
+
+    
